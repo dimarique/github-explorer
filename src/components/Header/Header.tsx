0971aa3cc5883c09c38e5e4061b70cfe418f3b12
dimarique <pochtagreen@gmail.com>
2025-12-04 15:36:28 +0100

    add viariables, replace main color theme, layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,21 +2,21 @@ import styles from "./Header.module.css";
 import { Github } from "lucide-react";
 
 interface HeaderProps {
-	// props?
+  // props?
 }
 
 const Header: React.FC<HeaderProps> = () => {
-	return (
-		<div className={styles.header}>
-			<div className={styles.left}>
-				<div className={styles.icon}>
-					<Github color="#ffffff" size={24} />
-				</div>
-				<h2 className={styles.header_text}>GitHub Explorer</h2>
-			</div>
-			<p>Discover amaizing developers</p>
-		</div>
-	);
+  return (
+    <div className={styles.header}>
+      <div className={styles.icon}>
+        <Github color="#ffffff" size={24} />
+      </div>
+      <div className={styles.right}>
+        <h1 className={styles.header_text}>GitHub Explorer</h1>
+        <p>Discover amaizing developers</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/MainInfo/MainInfo.tsx
+++ src/components/MainInfo/MainInfo.tsx
@@ -13,52 +13,62 @@ const MainInfo: React.FC<MainInfoProps> = ({ person }) => {
   return (
     person && (
       <div className={styles.user_info}>
-        <div className={styles.upper_part}>
-          <img className={styles.portrait} src={person?.avatar_url} alt="" />
-          <h1 className={styles.user_full_name}>{person?.name}</h1>
-          <h3 className={styles.user_login}>@{person?.login}</h3>
-          <p className={styles.user_bio}>{person?.bio}</p>
-          <div className={styles.user_footer}>
-            <div className={styles.footer_items}>
-              {person.location ? <MapPin size={16} /> : ""}
-              <p className={styles.user_location}>{person?.location}</p>
-            </div>
-            <div className={styles.footer_items}>
-              {person.blog ? <Link size={16} /> : ""}
-              <p className={styles.user_blog}>{person?.blog}</p>
-            </div>
-            <div className={styles.footer_items}>
-              <Calendar size={16} />
-              <p className={styles.user_joined}>
-                {person?.created_at &&
-                  `Joined ${new Date(person.created_at).toLocaleString(
-                    undefined,
-                    {
-                      month: "long",
-                      year: "numeric",
-                    },
-                  )}`}
-              </p>
-            </div>
+        <div className={styles.user_info_header}>
+          <img className={styles.user_image} src={person?.avatar_url} alt="" />
+          <div className={styles.user_name_block}>
+            <h1 className={styles.user_full_name}>{person?.name}</h1>
+            <h3 className={styles.user_login}>@{person?.login}</h3>
           </div>
         </div>
-        <div className={styles.lower_part}>
-          <div className={styles.user_metrics}>
-            <div className={styles.user_repositories}>
-              <BookOpen color="#782d92" />
-              <h1 className={styles.number}>{person?.public_repos}</h1>
-              <p className={styles.metric_name}>repositories</p>
-            </div>
-            <div className={styles.user_folowers}>
-              <Users color="#782d92" />
-              <h1 className={styles.number}>{person?.followers}</h1>
-              <p className={styles.metric_name}>followers</p>
-            </div>
-            <div className={styles.user_folowing}>
-              <Users color="#782d92" />
-              <h1 className={styles.number}>{person?.following}</h1>
-              <p className={styles.metric_name}>following</p>
-            </div>
+        <div className={styles.user_details}>
+          <p className={styles.user_bio}>{person?.bio}</p>
+          {person.location && (
+            <>
+              <div className={styles.user_details_item}>
+                <MapPin size={16} />
+                <p className={styles.user_location}>{person?.location}</p>
+              </div>
+            </>
+          )}
+          {person.blog && (
+            <>
+              <div className={styles.user_details_item}>
+                <Link size={16} />
+                <p className={styles.user_blog}>{person?.blog}</p>
+              </div>
+            </>
+          )}
+          {person.created_at && (
+            <>
+              <div className={styles.user_details_item}>
+                <Calendar size={16} />
+                <p className={styles.user_joined}>
+                  {`Joined
+                  ${new Date(person.created_at).toLocaleString(undefined, {
+                    month: "long",
+                    year: "numeric",
+                  })}
+                  `}
+                </p>
+              </div>
+            </>
+          )}
+        </div>
+        <div className={styles.user_metrics}>
+          <div className={styles.user_repositories}>
+            <BookOpen />
+            <h1 className={styles.number}>{person?.public_repos}</h1>
+            <p className={styles.metric_name}>repositories</p>
+          </div>
+          <div className={styles.user_folowers}>
+            <Users />
+            <h1 className={styles.number}>{person?.followers}</h1>
+            <p className={styles.metric_name}>followers</p>
+          </div>
+          <div className={styles.user_folowing}>
+            <Users />
+            <h1 className={styles.number}>{person?.following}</h1>
+            <p className={styles.metric_name}>following</p>
           </div>
         </div>
       </div>
