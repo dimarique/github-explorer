5214cac063a94fe33bdde8a2e3ed7fd46d2d9308
dimarique <pochtagreen@gmail.com>
2025-12-08 22:13:55 +0100

    fix error with searchWrapper className

--- src/components/search/SearchField/SearchField.tsx
+++ src/components/search/SearchField/SearchField.tsx
@@ -15,59 +15,61 @@ const SearchField: React.FC<SearchFieldProps> = ({ setInputValue }) => {
     setLocalValue(value);
   };
   return (
-    <div className={styles.search_wrapper}>
-      <div className={styles.searchField}>
-        <SearchInput
-          localValue={localValue}
-          handleInputChange={handleInputChange}
-          setInputValue={setInputValue}
-          setLocalValue={setLocalValue}
-        />
-        <SearchButton
-          localValue={localValue}
-          setInputValue={setInputValue}
-          setLocalValue={setLocalValue}
-        />
+    <>
+      <div className={styles.searchWrapper}>
+        <div className={styles.searchField}>
+          <SearchInput
+            localValue={localValue}
+            handleInputChange={handleInputChange}
+            setInputValue={setInputValue}
+            setLocalValue={setLocalValue}
+          />
+          <SearchButton
+            localValue={localValue}
+            setInputValue={setInputValue}
+            setLocalValue={setLocalValue}
+          />
+        </div>
+        <p className={styles.hint}>
+          Try searching:{" "}
+          {
+            <a
+              style={{ cursor: "pointer" }}
+              onClick={() => setInputValue("torvalds")}
+            >
+              torvalds
+            </a>
+          }
+          ,{" "}
+          {
+            <a
+              style={{ cursor: "pointer" }}
+              onClick={() => setInputValue("dimarique")}
+            >
+              dimarique
+            </a>
+          }
+          ,{" "}
+          {
+            <a
+              style={{ cursor: "pointer" }}
+              onClick={() => setInputValue("gaearon")}
+            >
+              gaearon
+            </a>
+          }
+          ,{" "}
+          {
+            <a
+              style={{ cursor: "pointer" }}
+              onClick={() => setInputValue("octocat")}
+            >
+              octocat
+            </a>
+          }
+        </p>
       </div>
-      <p className={styles.hint}>
-        Try searching:{" "}
-        {
-          <a
-            style={{ cursor: "pointer" }}
-            onClick={() => setInputValue("torvalds")}
-          >
-            torvalds
-          </a>
-        }
-        ,{" "}
-        {
-          <a
-            style={{ cursor: "pointer" }}
-            onClick={() => setInputValue("dimarique")}
-          >
-            dimarique
-          </a>
-        }
-        ,{" "}
-        {
-          <a
-            style={{ cursor: "pointer" }}
-            onClick={() => setInputValue("gaearon")}
-          >
-            gaearon
-          </a>
-        }
-        ,{" "}
-        {
-          <a
-            style={{ cursor: "pointer" }}
-            onClick={() => setInputValue("octocat")}
-          >
-            octocat
-          </a>
-        }
-      </p>
-    </div>
+    </>
   );
 };
 
