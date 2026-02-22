d31c2ef737bfbb4eebce8944231ae793dad65dd3
dimarique <pochtagreen@gmail.com>
2026-02-22 17:07:30 +0100

    Add commit hash to the Footer component

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { execSync } from "child_process";
 
-// https://vite.dev/config/
+const commitHash = execSync("git rev-parse --short HEAD").toString().trim();
+
 export default defineConfig({
+  define: {
+    __COMMIT_HASH__: JSON.stringify(commitHash),
+  },
   plugins: [
     react({
       babel: {
