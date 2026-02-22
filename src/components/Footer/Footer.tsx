310661047d65de84f59bcfa34b54c3dedce29183
dimarique <pochtagreen@gmail.com>
2026-02-22 17:07:30 +0100

    Add commit hash to the Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+declare const __COMMIT_HASH__: string;
+
+export const Footer = () => (
+  <footer>
+    {/* ... */}
+    <span style={{ opacity: 0.4, fontSize: 11 }}>{__COMMIT_HASH__}</span>
+  </footer>
+);
